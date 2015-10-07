66c0352ae05c74aa82c7766a88159edd87a631eb
stalingit <stalinmca2008@gmail.com>
2015-10-07 17:36:29 +0530

    Create tradingdialog.php

---
+++ tradingdialog.php
@@ -0,0 +1,8 @@
+\\clserver\development\stalin\football\application\views\user
+
+line no 22-----25
+
+   <!--  <input  type="file" class="btn btn-default" id="user_image" name="user_image" />-->
+                                 <span class="btn btn-default btn-file">
+                                          Browse <input type="file" id="user_image" name="user_image" accept="image/gif, image/jpeg,image/png">
+                                  </span>
