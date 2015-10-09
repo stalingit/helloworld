c9de64e659dd713d4f6f3059c23b1c9b249614da
stalingit <stalinmca2008@gmail.com>
2015-10-09 15:52:37 +0530

    Create change Files_09-10-15.php
    Trading dialog for Football site

---
+++ change Files_09-10-15.php
@@ -0,0 +1,17 @@
+1.\\CLSERVER\development\stalin\football\application\views\user\header.php
+
+2.\\CLSERVER\development\stalin\football\application\views\user\tradingdialog.php
+
+3.\\CLSERVER\development\stalin\football\assets\js\common_jquery.js
+
+4.\\CLSERVER\development\stalin\football\assets\css\football-web.css
+
+5.New file added
+    JS Folder
+  1.\\CLSERVER\development\stalin\football\assets\js\jquery.form.min.js
+  
+  2.\\CLSERVER\development\stalin\football\assets\js\sweetalert.min.js
+  
+  Css Folder:
+  
+  1.\\CLSERVER\development\stalin\football\assets\css\sweetalert.css
